bbb0bc466575a6447cb1ae1a40e3225b323911a4
MeghavatiChaudhari <meghapjc29@gmail.com>
2023-05-29 23:49:08 +0530

    signup form created

--- swift/src/components/Signup.js
+++ swift/src/components/Signup.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Signup=()=>{
    return(
-    <div>
-      Signup
+    <div className="container">
+      <br></br>
+      <br></br>
+      <h1>Sign up</h1>
+      <hr></hr>
+      <br></br>
+      <form className="form-group" autoComplete="off">
+        <label>Full Name</label>
+        <input type="text" className="form-control" required></input>
+        <br></br>
+        <br></br>
+        <label>Email</label>
+        <input type="email" className="form-control" required></input>
+        <br></br>
+        <br></br>
+        <label>Password</label>
+        <input type="password" className="form-control" required></input>
+        <br></br>
+         <div className="btn-box">
+          <span>Already have an account Login</span>
+          <Link to={'Login'} className="link">Here</Link>
+          <br></br>
+          <button className="btn btn-success btn-md"> SIGN UP</button>
+         </div>
+      </form>
     </div>
     
    )
