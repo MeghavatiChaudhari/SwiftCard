56eea5100b922a8be583974b6525b02ae59d6287
MeghavatiChaudhari <meghapjc29@gmail.com>
2023-06-04 20:22:41 +0530

    routing login signup pages created

--- swift/src/components/Login.js
+++ swift/src/components/Login.js
@@ -1,10 +1,31 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { Signup } from "./Signup";
 export const Login=()=>{
     return(
         <div>
-            Login
-            this is login page
+        <div className="container">
+      <br></br>
+      <br></br>
+      <h1>Login</h1>
+      <hr></hr>
+      <br></br>
+      <form className="form-group" autoComplete="off">
+        <label>Email</label>
+        <input type="email" className="form-control" required></input>
+        <br></br>
+        <br></br>
+        <label>Password</label>
+        <input type="password" className="form-control" required></input>
+        <br></br>
+         <div className="btn-box">
+          <span>Don't have an account Signup</span>
+          <Link to={'Signup'} className="link">Here</Link>
+          <br></br>
+          <button type="submit" className="btn btn-success btn-md"> SIGN UP</button>
+         </div>
+      </form>
+    </div>
         </div>
     )
 }
--- swift/src/components/Signup.js
+++ swift/src/components/Signup.js
@@ -1,21 +1,32 @@
+import { useState } from "react";
 import React from "react";
 import { Link } from "react-router-dom";
 
 export const Signup=()=>{
+
+     const[email ,setEmail] = useState('');
+     const[fullname, setFullname]= useState('');
+     const[password ,setPassword]=useState('');
+     const[errormsg ,setErrormsg] = useState('');
+     const[successmsg ,setSuccessmsg] = useState('');
+
+
+
+
    return(
     <div className="container">
       <br></br>
       <br></br>
-      <h1>Sign up</h1>
+      <h1>Signup</h1>
       <hr></hr>
       <br></br>
       <form className="form-group" autoComplete="off">
         <label>Full Name</label>
-        <input type="text" className="form-control" required></input>
+        <input type="text" className="form-control" required onChange={(e)=>setFullname(e.target.value)} value={fullname}></input>
         <br></br>
         <br></br>
         <label>Email</label>
-        <input type="email" className="form-control" required></input>
+        <input type="email" className="form-control" required onChange={(e)=>setEmail(e.target.value)} value={email}></input>
         <br></br>
         <br></br>
         <label>Password</label>
@@ -25,7 +36,7 @@ export const Signup=()=>{
           <span>Already have an account Login</span>
           <Link to={'Login'} className="link">Here</Link>
           <br></br>
-          <button className="btn btn-success btn-md"> SIGN UP</button>
+          <button type="submit" className="btn btn-success btn-md"> Login</button>
          </div>
       </form>
     </div>
