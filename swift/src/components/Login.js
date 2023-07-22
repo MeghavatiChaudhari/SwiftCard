0983df678e375b8405bf8066a50633b85204064b
MeghavatiChaudhari <meghapjc29@gmail.com>
2023-07-23 00:32:25 +0530

    login authenticate create

--- swift/src/components/Login.js
+++ swift/src/components/Login.js
@@ -1,15 +1,33 @@
 import React from "react";
 import { useState } from "react";
+import { auth } from "../config/config";
 import { NavLink } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { Signup } from "./Signup";
+import { useNavigate } from 'react-router-dom';
+
 export const Login=()=>{
+  const navigate = useNavigate(); 
 
      const[email,setEmail]=useState('');
      const[password,setPassword]=useState('');
+     const[successmsg , setSuccessmsg ] = useState('');
+     const[errormsg,setErrormsg]=useState('');
 
      const handleLogin=(e)=>{
       e.preventDefault();
-      console.log(email,password);
+      // console.log(email,password);
+      const auth = getAuth();
+       signInWithEmailAndPassword(auth, email, password).then((userCredential)=>{
+        setSuccessmsg("Successfully login!");
+        setEmail('');
+        setPassword('');
+        setErrormsg('');
+        setTimeout(()=>{
+          setSuccessmsg('');
+          navigate('/'); 
+        },3000);
+      }).catch(error=>setErrormsg(error.message));
      }
 
 
@@ -22,6 +40,10 @@ export const Login=()=>{
       {/* <h1>Login</h1> */}
       <hr></hr>
       <br></br>
+      {successmsg&&<>
+      <div className='succes-msg'>{successmsg}</div>
+       <br></br>
+       </>}
       <form className="form-group" autoComplete="off" onSubmit={handleLogin}>
         <label className="label">Email</label>
         <input type="email" className="form-control" required onChange={(e)=>setEmail(e.target.value)} value={email}></input>
@@ -37,6 +59,10 @@ export const Login=()=>{
           <button type="submit" className="button1"> Login</button>
          </div>
       </form>
+      {errormsg&&<>
+      <div className='error-msg'>{errormsg}</div>
+       <br></br>
+       </>}
       {/* <Signup /> */}
     </div>
         </div>
