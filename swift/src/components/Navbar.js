5308c93e50f1150501971999e9e0f8c6ddb21784
MeghavatiChaudhari <meghapjc29@gmail.com>
2023-07-24 00:38:49 +0530

    css for navbar

--- swift/src/components/Navbar.js
+++ swift/src/components/Navbar.js
@@ -2,15 +2,26 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { Signup } from "./Signup";
 import { Login } from "./Login";
+import logo from '../Images/logo.png';
 export const Navbar=()=>{
     return(
+      
+        <div className="navbar">
+
+        <div className="leftside">
+            <div className="logo">
+                <img src={logo} alt="logo"/>
+            </div>
+        </div>
         <div className="navigate">
         <span id="nav1">  
-        <NavLink to={"/Signup"} style={{color:"white"} }> SIGN UP </NavLink>
+         <NavLink to={"/Signup"} style={{color:"black"} }> SIGN UP </NavLink>
         </span>
         <span id="nav2">
-        <NavLink to={"/Login"} style={{color:"white"}}> LOGIN</NavLink>
-        </span>
+         <NavLink to={"/Login"} style={{color:"black"}}> LOGIN</NavLink>
+         </span>
         </div>
-    )
+
+      </div>
+    );
 }
